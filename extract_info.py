0c7f5acb3b2e4349bf4a9f7ad7aae39546c781d0
Julia Savochkina <j.savochkina@admitad.com>
2020-08-24 10:47:04 +0300

    updated extract_info.py

--- extract_info.py
+++ extract_info.py
@@ -1,12 +1,15 @@
 import json
 from typing import Generator
 
-from get_data import get_data
 
-
-def extract_info(type_of_search: str, issue_id: str) -> set:
+def extract_info(type_of_search: str, response_json: Generator) -> set:
+    """
+    Получает на вход гененратор с информацией одной страницы выдачи API. Достает для каждого event type_of_search и дату дейсвтия, если таковая имеется. Складывает в множество, чем гарантируется уникальность event'ов.
+    :param type_of_search: какое значение будет содержаться в ысм файле (пока это order_id или uid)
+    :param response_json: генератор с информацией одной страницы, получаем из get_data
+    :return: множество кортежей, одни кортеж - уникальное значение действия + дата (опционально)
+    """
     data: set = set()
-    response_json: Generator = get_data(issue_id)
     for issue in response_json:
         message: str = issue[0]['message']
         order_info: str = message.split('uid\n')[1]
@@ -20,4 +23,3 @@ def extract_info(type_of_search: str, issue_id: str) -> set:
 
         data.add(row)
     return data
-
